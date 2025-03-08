fe95712f8a08661e341697c27b5b1904c9f5e674
r021n <rozinabdul25@gmail.com>
2025-03-09 00:08:08 +0700

    remove unused routes, views, controllers, and email service for registration and home pages

--- src/controllers/home.controller.ts
+++
@@ -1,7 +0,0 @@
-import {Request, Response} from "express";
-import { getTotalRegistrations } from "../models/database.model";
-
-export const showHomePage = (req: Request, res: Response) => {
-    const totalRegistrations = getTotalRegistrations();
-    res.render("home", {totalRegistrations});
-}
--- src/routes/home.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { showHomePage } from "../controllers/home.controller";
-
-const router = Router();
-
-router.get("/", showHomePage);
-
-export default router;
